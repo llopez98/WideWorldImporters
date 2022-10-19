efdf01c11768604c7dd406ae0493dd37df0ae327
llopez <llopez@tropigas.com.pa>
2022-10-19 10:10:10 -0500

    Se configura las dependencias basicas del proyecto vue

--- wideworldimporters.vueapp/src/main.js
+++ wideworldimporters.vueapp/src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import VeeValidate from 'vee-validate'
 
 Vue.config.productionTip = false
+
+Vue.use(VeeValidate)
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
